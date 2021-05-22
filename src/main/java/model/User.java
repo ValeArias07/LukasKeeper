1c2ec16c624f3722b75dd58a29ce3ae7ba9fc5a5
Valentina Arias Parra <valeariasparra2001@gmail.com>
2021-05-21 19:33:57 -0500

    Obj user can be added in the db

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -6,58 +6,74 @@ import java.util.Date;
 
 public class User {
 
-	public static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    public static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
-	private String name;
-	private String lastName;
-	private Date dateOfBirth;
-	private String email;
-	private String password;
-	private String bank;
-	private String occupation;
+    private String name;
+    private String lastName;
+    private Date dateOfBirth;
+    private String email;
+    private String password;
+    private String bank;
+    private String occupation;
 
-	public User(){}
+    public User(){}
 
-	public User(String email, String password){
-		this.email = email;
-		this.password = password;
-	}
+    public User(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
 
-	public User(String name, String lastName, String dateOfBirth, String email, String password, String bank, String occupation) throws ParseException {
-		this.name = name;
-		this.lastName = lastName;
-		this.dateOfBirth = format.parse(dateOfBirth);
-		this.email = email;
-		this.password = password;
-		this.bank = bank;
-		this.occupation = occupation;
-	}
+    public User(String name, String lastName, String dateOfBirth, String email, String password, String bank, String occupation) throws ParseException {
+        this.name = name;
+        this.lastName = lastName;
+        this.dateOfBirth = format.parse(dateOfBirth);
+        this.email = email;
+        this.password = password;
+        this.bank = bank;
+        this.occupation = occupation;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public Date getDateOfBirth() {
-		return dateOfBirth;
-	}
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public String getBank() {
-		return bank;
-	}
+    public String getBank() {
+        return bank;
+    }
 
-	public String getOccupation() {
-		return occupation;
-	}
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setLastName(String lastname){
+        this.lastName=lastname;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", bank='" + bank + '\'' +
+                ", occupation='" + occupation + '\'' +
+                '}';
+    }
 }
--- src/main/java/providers/UserProvider.java
+++ src/main/java/providers/UserProvider.java
@@ -9,19 +9,19 @@ public class UserProvider {
 
     public void addUser(User user){
         try {
-
             DBConnection connection = new DBConnection();
            String date =User.format.format(user.getDateOfBirth());
-           System.out.print(User.format.format(user.getDateOfBirth()));
+           System.out.println(user.getLastName());
            String sql = ("INSERT INTO users(name, lastname, email, password, dateOfbirth, bank, occupation) " +
-                    "VALUES ($NAME,$LASTNAME,$EMAIL, $PASSWORD,$BORNDATE, $BANK,$OCCUPATION")
+                    "VALUES ($NAME,$LASTNAME,$EMAIL, $PASSWORD,$BORNDATE, $BANK,$OCCUPATION)")
                     .replace("$NAME","'"+user.getName()+"'")
                     .replace("$LASTNAME", "'"+user.getLastName()+"'")
-                    .replace("$EMAIL","'"+ user.getEmail()+"'")
-                    .replace("$PASSWORD","'"+ user.getPassword()+"'")
-                    .replace("$BORNDATE", "'" + date + "'")
+                    .replace("$EMAIL","'"+user.getEmail()+"'")
+                    .replace("$PASSWORD","'"+user.getPassword()+"'")
+                    .replace("$BORNDATE", "'"+date+"'")
                     .replace("$BANK","'"+ user.getBank()+"'")
                     .replace("$OCCUPATION", "'"+user.getOccupation()+"'");
+            connection.connect();
             connection.commandSQL(sql);
             connection.disconnect();
         } catch (SQLException throwables) {
