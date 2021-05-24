3b5f9c6c032a3305c26460f91612bbd018e32999
Jhon Saldarriaga <jhonsaldarriagalopez@gmail.com>
2021-05-23 19:56:23 -0500

    The model has been fixed

---
+++ src/main/java/model/ChangesInAssets.java
@@ -0,0 +1,69 @@
+package model;
+
+
+import java.util.Date;
+
+public class ChangesInAssets {
+
+	private int id;
+	private double value;
+	private String description;
+	private Date date;
+	private String frequency;
+	private int idCategory;
+	private int idUser;
+
+
+	public ChangesInAssets(){}
+
+	public ChangesInAssets(int id, double value, String description, Date date, String frequency, int idCategory, int idUser) {
+		this.id = id;
+		this.value = value;
+		this.description = description;
+		this.date = date;
+		this.frequency = frequency;
+		this.idCategory = idCategory;
+		this.idUser = idUser;
+	}
+
+	public ChangesInAssets(double value, String description, Date date, String frequency, int idCategory, int idUser) {
+		this.value = value;
+		this.description = description;
+		this.date = date;
+		this.frequency = frequency;
+		this.idCategory = idCategory;
+		this.idUser = idUser;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+	public int getIdCategory() {
+		return idCategory;
+	}
+
+	public int getIdUser() {
+		return idUser;
+	}
+}
--- src/main/java/providers/MySqulMethodsInterface.java
+++
@@ -1,8 +0,0 @@
-package providers;
-
-public interface MySqulMethodsInterface {
-    public void add(String obj);
-    public void update(int id);
-    public void delete(int id);
-    public void stadistics(String type);
-}
