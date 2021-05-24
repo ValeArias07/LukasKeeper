e44298aeb239471cefae846813ae17dc15961b3b
JLRO1812 <jose.restrepo2@correo.icesi.edu.co>
2021-05-24 00:04:43 -0500

    fix and data acquisition

---
+++ src/main/webapp/user/add/js/addDebt.js
@@ -0,0 +1,15 @@
+const value = document.getElementById('amount');
+const description = document.getElementById('description');
+const fee = document.getElementById('numFee');
+const interest = document.getElementById('interestPer');
+const button = document.getElementById('buttonContainer');
+
+const addIncome=()=>{
+
+    let xhr = new XMLHttpRequest();
+    const debts = new Debt(value.value, description.value, fee.value, interest.interestPer);
+    xhr.open("POST", "http://localhost:8081/LukasKeeper_war/api/debts/add");
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(debts));
+    alert("Deuda añadida!");
+}
---
+++ src/main/webapp/user/add/js/debt.js
@@ -0,0 +1,8 @@
+class Debt{
+    constructor(value, description, fee, interest){
+        this.value=value;
+        this.description=description;
+        this.fee=fee;
+        this.interest=interest;
+    }
+}
