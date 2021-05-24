009529dc1682036161c34dd90751e0ad88e93795
JLRO1812 <jose.restrepo2@correo.icesi.edu.co>
2021-05-23 23:05:18 -0500

    Data acquisition, expense

---
+++ src/main/webapp/user/add/js/addExpense.js
@@ -0,0 +1,20 @@
+const amount = document.getElementById('amount');
+const description = document.getElementById('description');
+const idCategory = document.getElementById('expenseType');
+const button = document.getElementById('buttonContainer');
+
+const getSelectors=()=>{
+    let index1= document.getElementById("idCategory").selectedIndex;
+    var idCategory=document.getElementById("idCategory").options[index1].text;
+    addIncome(idCategory);
+}
+
+const addExpense=(idCategory)=>{
+
+    let xhr = new XMLHttpRequest();
+    const expense = new Income(amount.value, description.value, idCategory);
+    xhr.open("POST", "http://localhost:8081/LukasKeeper_war/api/api/expenses/add");
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(expense));
+    alert("Ingreso añadido!");
+}
---
+++ src/main/webapp/user/add/js/expense.js
@@ -0,0 +1,7 @@
+class Expense{
+    constructor(amount, description, idCategory){
+        this.amount=amount;
+        this.description=description;
+        this.idCategory=idCategory;
+    }
+}
