cdb67cdf84f91d9bed70992c711dc56050a9f095
ValeArias07 <1000255081@u.icesi.edu.co>
2021-05-24 22:30:30 -0500

    Window add tested and fixed

--- src/main/webapp/user/add/js/addExpense.js
+++ src/main/webapp/user/add/js/addExpense.js
@@ -1,20 +1,29 @@
 const value = document.getElementById('amount');
 const description = document.getElementById('description');
-const idCategory = document.getElementById('expenseType');
-const button = document.getElementById('buttonContainer');
+const date = document.getElementById('date');
+const frequency = document.getElementById('frequencyType');
+const button = document.getElementById('button');
 
 const getSelectors = () => {
-    let index1 = document.getElementById("idCategory").selectedIndex;
-    var idCategory = document.getElementById("idCategory").options[index1].text;
-    addExpense(idCategory);
+    let index1 = document.getElementById("expenseType").selectedIndex;
+    let idCategory = document.getElementById("expenseType").options[index1].text;
+
+    let index2 = document.getElementById("frequencyType").selectedIndex;
+    let frequencyType = document.getElementById("frequencyType").options[index2].text;
+
+    addExpense(idCategory, frequencyType);
 }
 
-const addExpense = (idCategory) => {
+const addExpense = (idCategory, frequencyType) => {
 
     let xhr = new XMLHttpRequest();
-    const expense = new Expense(value.value, description.value, idCategory);
-    xhr.open("POST", "http://localhost:8081/LukasKeeper_war/api/expenses/add");
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(expense));
+    const expense = new Expense(value.value, description.value, date.value, frequencyType, idCategory);
+
+    console.log( JSON.stringify(expense));
+    //xhr.open("POST", "http://localhost:8081/LukasKeeper_war/api/expenses/add?email=bengi1234@gmail.com");
+    //xhr.setRequestHeader('Content-Type', 'application/json');
+    //xhr.send(JSON.stringify(expense));
     alert("Gasto añadido!");
 }
+
+button.addEventListener('click', getSelectors);
