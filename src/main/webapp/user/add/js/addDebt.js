44a3afc8f2552fd8ce580c04382c3babc8fb2727
Jhon Saldarriaga <jhonsaldarriagalopez@gmail.com>
2021-05-25 18:35:57 -0500

    LocalStorage for Login has been implemented, also, the Front was fixed and integrated with the Back

--- src/main/webapp/user/add/js/addDebt.js
+++ src/main/webapp/user/add/js/addDebt.js
@@ -10,9 +10,10 @@ const addDebt=()=>{
     let xhr = new XMLHttpRequest();
     const debts = new Debt(value.value, description.value, date.value, fee.value, interest.value);
     console.log(JSON.stringify(debts));
-   // xhr.open("POST", "http://localhost:8081/LukasKeeper_war/api/debts/add");
-   // xhr.setRequestHeader('Content-Type', 'application/json');
-    //xhr.send(JSON.stringify(debts));
+    let session = JSON.parse(window.localStorage.getItem('session'));
+    xhr.open("POST", "http://localhost:8081/LukasKeeper_war/api/debts/add?email="+session.email);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(debts));
     alert("Deuda añadida!");
 
 }
