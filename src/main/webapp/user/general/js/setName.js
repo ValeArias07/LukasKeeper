012bc60f6bfd6ba1c6f3a9e32de56dd708130aa7
ValeArias07 <1000255081@u.icesi.edu.co>
2021-05-26 21:05:53 -0500

    Profile js implemented

---
+++ src/main/webapp/user/general/js/setName.js
@@ -0,0 +1,21 @@
+const session = JSON.parse(window.localStorage.getItem('session'));
+
+setName =()=>{
+
+        let xhr = new XMLHttpRequest();
+        xhr.addEventListener('readystatechange',()=>{
+            if(xhr.readyState===4){
+                let json = xhr.responseText;
+                var user= JSON.parse(json);
+                window.localStorage.setItem('name', user.name);
+            }
+        });
+
+        xhr.open("GET", "http://localhost:8081/LukasKeeper_war/api/user/getUserInfo?email="+session.email);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+
+        console.log(localStorage.getItem('name'));
+    document.getElementById('nameModel').value=localStorage.getItem('name');
+}
+setName();
