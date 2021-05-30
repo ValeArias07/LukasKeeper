a7599ddf65c372238bb94ffc64b5ed8f97051460
ValeArias07 <1000255081@u.icesi.edu.co>
2021-05-29 20:42:42 -0500

    Fronted Simplified 2.0

--- src/main/webapp/js/logOut.js
+++ src/main/webapp/js/logOut.js
@@ -1,5 +1,5 @@
-const button = document.getElementById('logOutButton');
-button.addEventListener('click',   logut=() => {
+const bLogOut = document.getElementById('logOutButton');
+bLogOut.addEventListener('click',   logut=() => {
     window.localStorage.removeItem('session');
     window.localStorage.removeItem('name');
     window.location.href = '../../index.html';
