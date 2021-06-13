c70f2c9f4e9eba518043ad473d6d2c6c1fc1a18e
ValeArias07 <1000255081@u.icesi.edu.co>
2021-06-13 17:25:18 -0500

    loadButtons deleted

--- src/main/webapp/user/categories/js/loadButtonsExpenses.js
+++ src/main/webapp/user/categories/js/loadButtonsExpenses.js
@@ -1,15 +1,1 @@
-const addBtn = document.getElementById('addBtn');
-const periodBtn = document.getElementById('periodBtn');
-const compareBtn = document.getElementById('compareBtn');
 
-goToAdd=()=>{
-    window.location="../add/addExpenses.html";
-}
-
-goToMain = () => {
-    window.location="../add/addExpenses.html";
-}
-
-addBtn.addEventListener('click', goToAdd);
-periodBtn.addEventListener('click', goToMain);
-compareBtn.addEventListener('click', goToMain);
