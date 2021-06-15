4483863f05082fdcf7c2c6c2636087bb617a628e
Danna García <1193251859@u.icesi.edu.co>
2021-06-14 22:40:55 -0500

    loadExpensesElements.js creado

--- src/main/webapp/user/categories/js/expensesConfig.js
+++ src/main/webapp/user/categories/js/expensesConfig.js
@@ -4,6 +4,38 @@ const periodBtn = document.getElementById('periodBtn');
 const timelineBtn = document.getElementById('timelineBtn');
 const compareBtn = document.getElementById('compareBtn');
 const indicatorsBtn = document.getElementById('indicatorsBtn');
+const entretenimientoInformacion = document.getElementById('entretenimientoInformacion');
+const saludInformacion = document.getElementById('saludInformacion');
+
+const getAllExpenses = () =>{
+    let session = JSON.parse(window.localStorage.getItem('session'));
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', ()=>{
+        if(xhr.readyState === 4){
+            let json = xhr.responseText;
+            let response = JSON.parse(json);
+            console.log(response);
+            entretenimientoInformacion.innerHTML = '';
+            saludInformacion.innerHTML = '';
+
+            for(let i = 0; i<response.length;i++){
+                let expenses = response[i];
+                let view = new loadExpensesElements(debts);
+                view.onDeleteFinish = () =>{
+                    //if(expenses.id)
+                    debtsInformation.removeChild(document.getElementById('debts' + debts.id));
+                };
+                debtsInformation.appendChild(view.render());
+
+            }
+        }
+    });
+    xhr.open('GET', 'http://localhost:8081/LukasKeeper_war/api/expenses/list?email=' + session.email);
+    xhr.send();
+};
+
+getAllExpenses();
+
 
 
 loadContainerAdd=()=>{
