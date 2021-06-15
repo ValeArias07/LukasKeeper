92917b0ec4a49ef01d0246a82e69402a82b9c9a2
ValeArias07 <1000255081@u.icesi.edu.co>
2021-06-15 04:17:00 -0500

    fixes

--- src/main/webapp/user/general/js/dashboard.js
+++ src/main/webapp/user/general/js/dashboard.js
@@ -1,23 +1,10 @@
-const saldov = document.getElementById('saldov');
-const ingresov = document.getElementById('ingresov');
-const gastosv = document.getElementById('gastosv');
+let saldov = document.getElementById('saldov');
+let ingresov = document.getElementById('ingresov');
+let gastosv = document.getElementById('gastosv');
+let session = JSON.parse(window.localStorage.getItem('session'));
 
-renderInfoSaldo=()=>{
-    let xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange',()=>{
-       if(xhr.readyState==4){
-           let json = xhr.responseText;
-           let response = JSON.parse(json);
-           console.log(response);
-
-            saldov.innerHTML = response.toString();
-       }
-    });
-    xhr.open('GET','inserteurldelservicio');
-    xhr.send();
-}
-
-renderInfoSaldo();
+var ingreso=0;
+var gasto=0;
 
 renderInfoIngreso=()=>{
     let xhr = new XMLHttpRequest();
@@ -25,30 +12,34 @@ renderInfoIngreso=()=>{
         if(xhr.readyState==4){
             let json = xhr.responseText;
             let response = JSON.parse(json);
-            console.log(response);
 
             ingresov.innerHTML = response.toString();
+            ingreso=response;
+            console.log(ingreso);
+            let op=(ingreso-gasto);
+            console.log(op);
+            saldov.innerHTML=op;
         }
     });
-    xhr.open('GET','inserteurldelservicio');
+    xhr.open('GET','http://localhost:8081/LukasKeeper_war/api/expenses/getBalanceIncome?email='+session.email);
     xhr.send();
 }
-
-renderInfoIngreso();
 
 renderInfoGasto=()=>{
     let xhr = new XMLHttpRequest();
     xhr.addEventListener('readystatechange',()=>{
         if(xhr.readyState==4){
+
             let json = xhr.responseText;
             let response = JSON.parse(json);
             console.log(response);
-
-            gastosv.innerHTML = response.toString();
+            gasto=(-1)*response;
+            gastosv.innerHTML = (-1)*response;
+            renderInfoIngreso();
         }
     });
-    xhr.open('GET','inserteurldelservicio');
+
+    xhr.open('GET','http://localhost:8081/LukasKeeper_war/api/expenses/getBalanceExpenses?email='+session.email);
     xhr.send();
 }
-
 renderInfoGasto();
